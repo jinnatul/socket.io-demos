8c7a0e2e4bf57aae005e3c3e6d290b12a1c46a7a
jinnatul <morolswediu@gmail.com>
2021-02-12 12:56:24 +0600

    create & connect socket between server and client

--- chat-ejs/app.js
+++ chat-ejs/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import http from 'http';
+import socketIO from 'socket.io';
 
 const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -9,4 +13,11 @@ app.get('/', (req, res) => {
   res.render('index');
 })
 
-export default app;
+io.on('connection', (socket) => {
+  console.log('user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+})
+
+export default server;
